371a17e168d9c9d22a84c4b025d848cab073a362
= <=>
2025-04-26 16:14:53 -0500

    subiendo

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base : "https://github.com/juanca1620.github.io/FrontEnd-Vank-FInal",
 })
